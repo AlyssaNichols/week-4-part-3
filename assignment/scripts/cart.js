5c9938e46353dd8b37f3a27f19ace66f9a64c0af
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-08-08 15:35:22 -0500

    wrote out var and function names, haven't completed anything really

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,12 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem()
+
+function listItems()
+
+function empty()
+
