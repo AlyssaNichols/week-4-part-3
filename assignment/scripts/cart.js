40604f6dc866e1ea8b4022ba1ebd72ed92ad7917
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-08-10 15:21:49 -0500

    completed stretch goals 1-3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,8 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+
+const maxItems = 5;
 let basket = [];
 
 function addItem(item){
@@ -36,3 +38,38 @@ console.log('is basket empty?', empty(basket));
 
 console.log(basket);
 
+//basket is reset back to an empty array
+
+function isFull(){
+    if (basket.length >= maxItems){
+        return true
+    } else {
+        return false
+    }
+}
+console.log("Is the basket full? (should return false)", isFull());
+
+
+
+function addItem(item){
+    if (basket.length-1 < maxItems) {
+        console.log('added item:', item)
+        basket.push(item);
+        return true;
+    } else {
+        return false
+    }
+}
+
+// filled basket back up with fruit using the addItem function
+
+console.log(addItem("apple")); // Output: true
+console.log(addItem("banana")); // Output: true
+console.log(addItem("orange")); // Output: true
+console.log(addItem("grape"));  // Output: true
+console.log(addItem("kiwi"));   // Output: true
+console.log(addItem("melon")); // no room, Output: false
+
+console.log("Is the basket full? (should return true)", isFull());
+
+// couldn't figure out the last stretch goal but will continue to look at it and try to understand what it's asking for.
